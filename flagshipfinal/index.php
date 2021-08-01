c8a00ab8ae4909fbfe2e535111d9d8675b206f5a
Deepak Patil <patildeep2000@gmail.com>
2021-08-02 01:48:28 +0530

    flagship added

---
+++ flagshipfinal/index.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Flagship | E-Cell VNIT</title>
+    <link href="ecellcss/overlay.css" rel="stylesheet">
+    <link href="ecellcss/global/global.css" rel="stylesheet">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- fontAwesome -->
+    <script src="https://kit.fontawesome.com/f237bf30a2.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- googlw fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- style -->
+    <link rel="stylesheet" href="css/share.css">
+    <link rel="stylesheet" href="css/formStyle.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <!-- favicon  -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<!-- nav section start -->
+<nav id="mainNavbar" class="navbar navbar-expand-lg navbar-light bg-white shadow sticky-top">
+      <div class="container">
+      <a class="navbar-brand ml-0" href="index.php"><img src="img/logo-ecell-sm.png" alt="Ecell Logo" ></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="nav navbar-nav ms-auto">
+            <li class="nav-item active">
+              <a class="nav-link active" href="https://www.ecellvnit.org/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.ecellvnit.org/#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.ecellvnit.org/team.php">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="https://www.ecellvnit.org/spons.php">Sponsors</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#myBlog">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.ecellvnit.org/#contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    <!-- nav section end -->
+<section class="bg_img">
+    <div class="container">
+      <div class="row align-items-center text-md-left min-vh-100">
+        <div class="">
+          <h1 id="myName" style="color:white" class="my-0 head-size">FLAGSHIP'21</h1>
+          <a class="btn btn-lg myColor text-white rounded-pill pl-5 pr-5 pt-2 pb-2" href="#register-here">Register</a> 
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- </div>
+  </section> -->
+  <br><br><br>
+  <div class="content_img">
+  <div class="container mt-5">
+    <div class="row my-2">
+      <div class="col-md-6 col-sm-12 para-font">
+        <p style="" class="para  ml-0 pt-5 pb-5 mt-5">Flagship connects today's youth to intellectual and experienced speakers to
+            help them
+            achieve their purpose
+            by sharing their stories of hustle and perseverance and the lessons they learnt on their path to
+            glory.</br> <br>
+            As the first event of the year, Flagship leaves a lasting impact on the minds of the students through a
+            motivating and 
+            brexciting session. They get to explore new avenues presented by the speakers and fine tune their thought
+            process. It
+            provides students a platform to display innovation and address issues of national and international
+            importance.</p>
+        </p>
+      </div>
+      <!-- <div class="col-md-4 d-md-block d-none">
+        <img src="img/Flagshiplogo.png"alt=".." class="w-100">
+      </div> -->
+    </div>
+  </div>
+    </div>
+  </div>
+</div>
+  <br><br>
+
+<!-- Speakers's Cards start -->
+<div class="blog-card-container">
+  <div class="row">
+        <div class="col-md-12 g-margin-t-15--xs g-margin-b-60--xs g-margin-b-0--lg">
+            <div class="underline wow fadeInLeft g-text-center--xs" data-wow-duration=".3" data-wow-delay=".3s">
+                <h3 class="text-center g-font-size-32--xs g-font-size-36--sm g-font-size-36--md ">Speakers <span class="g-color--primary" style="color:#4060B5">Flagship'21</span></h3>
+            </div>
+            <hr class="m-auto" style="height:3px; width:40%; border-width:0; color:#4060B5; background-color:#4060B5">
+        </div>
+    </div>
+    <br>
+  <!-- Speaker 1 -->
+    <div class="blog-card jumbotron p-4 p-md-5 text-black rounded bg-light shadow">
+      <div class="row pb-2">
+        <div class="col-md-4 m-auto text-center">
+          <!-- <img class="blog-image" alt="cat and dog" src="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_960_720.jpg"> -->
+          <img class="blog-image " style="border-radius:50%" alt="speaker-1" src="img/karan shah.png">
+        </div>
+
+        <div class="blog-desc col-md-8 ">          
+        <h2 class="speaker-font"><span style="color:#4060B5" class="">Karan Shah</span></h2>
+          <p class="lead my-3 new-speaker-font text-left">He is the founder of The Indian Institute of Digital Education. The organization has impacted more than 40,000 students and large corporates online and offline. He aims to change the way education is delivered in the country by making it more fun, practical and experience-based.
+            He was a student at NMIMS College & also studied Private Equity & E-Commerce Strategies at Harvard University.
+            Karan is also a Tedx speaker and a frequent speaker at the IIMs in India. 
+          </p>
+          <a style="font-size:12px" class="btn align-items-center btn-lg myColor text-white rounded-pill pl-5 pr-5 pt-2 pb-2" href="#register-here">Free Registration</a>
+        </div>
+      </div>
+    </div>
+  <!-- Speaker 2 -->
+    <!-- <div class="blog-card jumbotron p-4 p-md-5 text-black rounded bg-light">
+      <div class="row">
+        <div class="col-md-4">
+          <img class="blog-image" alt="speaker-1" src="img/harsh-goela.png">
+        </div>
+        <div class="blog-desc col-md-8 ml-auto my-auto pt-lg-0 pt-3">          
+        <h2 class="speaker-font"><span style="color:#4060B5" class="">Karan Shah</span></h2>
+          <p class="lead my-3 new-speaker-font">He is the founder of The Indian Institute of Digital Education. The organization has impacted more than 40,000 students and large corporates online and offline. He aims to change the way education is delivered in the country by making it more fun, practical and experience-based.
+            He was a student at NMIMS College & also studied Private Equity & E-Commerce Strategies at Harvard University.
+            Karan is also a Tedx speaker and a frequent speaker at the IIMs in India. 
+          </p>
+        </div>
+      </div>
+    </div> -->
+  
+  </div>
+
+
+<!-- Speaker's Cards end -->
+
+
+
+
+
+
+  <div class="container g-padding-y-10--xs g-padding-y-40--sm" style="background: rgba(255, 255, 255, 1);">
+    <div class="row">
+        <div class="col-md-12 g-margin-t-15--xs g-margin-b-60--xs g-margin-b-0--lg">
+            <div class="underline wow fadeInLeft g-text-center--xs" data-wow-duration=".3" data-wow-delay=".3s">
+                <h3 class="text-center g-font-size-32--xs g-font-size-36--sm g-font-size-36--md ">Past <span class="g-color--primary" style="color:#4060B5">Speakers</span></h3>
+            </div>
+            <hr class="m-auto" style="height:3px; width:40%; border-width:0; color:#4060B5; background-color:#4060B5">
+        </div>
+    </div>
+    
+    <div class="row mt-3 " id="hex-guests">
+        <div class="col-md-6 col-sm-6 text-center">
+            <img src="img/harsh-goela.png" class="hex"></img>
+            <h2 class="speaker-font"><span style="color:#4060B5" class="">Harsh Goela</span></h2>
+            <p class="">Stock Trader<br><b>TEDx</b> Speaker, Founder of Goela School of Finance</p>
+        </div>
+        <div class="col-md-6 col-sm-6 text-center mb-4">
+            <img src="img/pankhuri.png" class="hex"></img>
+            <h2 class="speaker-font"><span style="color:#4060B5" class="g-color--primary">Pankhuri Gidwani</span></h2>
+            <p class="">Miss Grand International India 2016<br>Renowned <b>TEDx</b> Speaker</p>
+        </div>
+        <div class="col-md-6 col-sm-6 m-auto text-center">
+            <img src="img/jay-alani.png" class="hex"></img>
+            <h2 class="speaker-font "><span style="color:#4060B5" class="g-color--primary">Jay Alani</span></h2>
+            <p class="">Paranormal Investigator<br>Renowned <b>TEDx</b> Speaker</p>
+        </div>
+    </div>
+
+</div>
+
+    <section id="register-here" class="register-box">
+        <div class="text-center pt-4 form-heading">
+            <h2>Register Now!</h2>
+        </div>
+        <div class="container register pb-5">
+            <div class="row  mt-5">
+                <form method="post" action="store_details.php">
+                    <!-- <div class="row"> -->
+                        <!-- name field  -->
+                        <div class="form__group field ">
+                            <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+                            <label for="name" class="form__label ">Full Name *</label>
+                        </div>
+                        <!-- contact field  -->
+                        <div class=" d-flex justify-content-between">
+                            <div class="form__group field col-md-5 me-2">
+                                <input type="input" class="form__field" placeholder="Email" name="email" id='email' required />
+                                <label for="email" class="form__label">Email ID *</label>
+                            </div>
+                            <div class="form__group field col-md-6 ms-2 ">
+                                <input type="input" class="form__field " placeholder="Contact" name="contact" id='contact' required />
+                                <label for="contact" class="form__label">Contact No. *</label>
+                            </div>
+                        </div>
+                        <p class="my-2">Select your speakers</p>
+                        <!-- checkbox field  -->
+                        <div class="container">
+                          <div class="row">
+                            <!-- <div class=" btn-group" role="group" aria-label="Basic checkbox toggle button group"> -->
+                                <div class="checkbox-style text-center col-md-4 mt-2 col-sm-12">
+                                <input type="checkbox" class="btn-check" id="speakers1" name="speakers[]" value="speaker1" autocomplete="off">
+                                <label class="btn btn-color" for="speakers1">Checkbox 1</label>
+                                </div>
+
+                                <div class="checkbox-style text-center col-md-4 col-sm-12 mt-2 mt-sm-2">
+                                <input type="checkbox" class="btn-check" id="speakers2" name="speakers[]" value="speaker2"  autocomplete="off">
+                                <label class="btn btn-color" for="speakers2">Checkbox 2</label>
+                                </div>
+
+                                <div class="checkbox-style text-center col-md-4 col-sm-12 mt-2 mt-sm-2">
+                                <input type="checkbox" class="btn-check" id="speakers3" name="speakers[]" value="speaker3" autocomplete="off">
+                                <label class="btn btn-color" for="speakers3">Checkbox 3</label>
+                                </div>
+                            <!-- </div> -->
+                          </div>
+                        </div>
+                        <!-- college field  -->
+                        <div class="form__group field col-md-12">
+                            <input type="input" class="form__field" placeholder="College" name="college" id='college' required />
+                            <label for="college" class="form__label">College Name *</label>
+                        </div>
+                    <!-- </div> -->
+                    <div class="text-center mt-4 btn-style">
+                        <button class=" btn  rounded-pill pl-5 pr-5 pt-2 pb-2" type="submit" name="submit_form" value="SUBMIT">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    
+  <!---------------------------- FOOTER START ------------------------------------------->
+ 	<footer class="g-bg-color--dark">
+
+<!-- Copyright -->
+<div class="container g-padding-y-30--xs">
+  <h1 class="g-color--white-opacity">Quick Links</h1>
+  <br>
+  <div class="footer-row">
+
+    <div class="col-sm-12 col-md-3 col-lg-3">
+      <h4 class="g-color--white-opacity"><b>Consortium'21</b></h4>
+      <a href="https://consortium.ecellvnit.org/" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+        Consortium, the Annual Entrepreneurship Summit of VNIT Nagpur
+      </a>
+    </div>
+
+    <div class="col-sm-12 col-md-3 col-lg-3">
+      <h4 class="g-color--white-opacity"><b>Startup Conclave</b></h4>
+      <a href="https://startupconclave.ecellvnit.org/" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+        StartUp Conclave, a national-level business plan competition and a part of Consortium'21
+      </a>
+    </div>
+
+    <div class="col-sm-12 col-md-3 col-lg-3">
+      <h4 class="g-color--white-opacity"><b>Buy Merchandise</b></h4>
+      <a href="https://store.ecellvnit.org/" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+        Buy official merchandise of Consortium'21 at Ecell VNIT online store.
+      </a>
+    </div>
+    <div class="col-sm-12 col-md-3 col-lg-3">
+      <h4 class="g-color--white-opacity"><b>Startup Expo</b></h4>
+      <a href="https://consortium.ecellvnit.org/expo.php" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+        A grand platform for startups to display their product and/ or service to all the people.
+      </a>
+    </div>
+
+  </div>
+</div>
+<br>
+
+
+<div class="container" style="padding-top:22px">
+
+  <div class="footer-row">
+
+    <div class="col-md-3">
+      <div>
+        <img src="https://startupconclave.ecellvnit.org/static/img/E-Cell_white.png" alt="" height="80" style="padding-top:10px">
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <h4 class="g-color--white-opacity">Contact Us</h4>
+      <p class="g-color--white-opacity-light">For Association Opportunities:<br><strong>contact@ecellvnit.org</strong></p>
+      <p class="g-color--white-opacity-light">For Sponsorship Opportunities:<br><strong>corporate@ecellvnit.org</strong></p>
+    </div>
+
+    <div class="col-md-3">
+      <h4 class="follow-ecell">Follow Ecell VNIT</h4>
+
+      <div class="row share-row justify-content-center" id="share_ecell">
+
+        <div class="col-md-4 col-2" id="share_ecell_icon">
+          <a class="s-header__action-link" href=" https://m.facebook.com/vnitecell/" target="_blank">
+            <span id="icons_ecell"><i class="fab fa-facebook fa-2x"></i></span>
+            <span class="aria-hidden sr-only">Facebook</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-2" id="share_ecell_icon">
+          <a class="s-header__action-link" href="https://twitter.com/ecell_vnit" target="_blank">
+            <span id="icons_ecell"><i class="fab fa-twitter fa-2x"></i></span>
+            <span class="aria-hidden sr-only">Twitter</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-2" id="share_ecell_icon">
+          <a class="s-header__action-link" href=" https://www.instagram.com/ecellvnit/" target="_blank">
+            <span id="icons_ecell"><i class="fab fa-instagram fa-2x"></i></span>
+            <span class="aria-hidden sr-only">Instagram</span>
+          </a>
+        </div>
+    </div>
+    <div class="row share-row justify-content-center" id="share_ecell">
+        <div class="col-md-4 my-3 col-2" id="share_ecell_icon ecell_icon_bottom">
+          <a class="s-header__action-link" href=" https://www.linkedin.com/company/6615520/" target="_blank">
+            <span id="icons_ecell"><i class="fab fa-linkedin-in fa-2x"></i></span>
+            <span class="aria-hidden sr-only">LinkedIn</span>
+          </a>
+        </div>
+        <div class="col-md-4 my-3 col-2" id="share_ecell_icon ecell_icon_bottom">
+          <a class="s-header__action-link" href=" https://www.youtube.com/channel/UC0KNOmODhqLcEpcfN9qEsIQ" target="_blank">
+            <span id="icons_ecell"><i class="fab fa-youtube fa-2x"></i></span>
+            <span class="aria-hidden sr-only">YouTube</span>
+          </a>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-md-3">
+      <h4 class="g-color--white-opacity">Our Hosting Partner</h4>
+      <div class="hosting-partner-image-container">
+        <a href="https://www.znetlive.com/">
+          <img src="ecellimg/znetlive.png" alt="hosting partner - znet">
+        </a>
+      </div>
+    </div>
+
+  </div>
+  <div class="row">
+    <div class="col-sm-12" style="position:relative; text-align:center">
+      <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white-opacity g-padding-y-20--md">&copy;
+        <a class="g-color--primary" href="https://ecellvnit.org" target="_blank">E-Cell VNIT</a>
+      </p>
+
+    </div>
+  </div>
+
+</div>
+<!-- End Copyright -->
+</footer>
+<!----------------------------------FOOTER END---------------------------->
+
+
+    <!-- Optional JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
