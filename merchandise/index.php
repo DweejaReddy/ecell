70d5dc50e287720ccc6bd84995e224753cfe7500
asachkahitari <patildeep2000@gmail.com>
2022-01-23 17:31:25 +0530

    merch page

---
+++ merchandise/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Merchandise | E-CELL, VNIT</title>
+</head>
+<?php include('header.php')?>
+<body style="background-color: #232323;">
+    <div class="bord d-none d-xl-block"></div>
+    <div class="name d-none d-xl-block">E-CELL VNIT</div>
+    <div class="bord1 d-none d-xl-block"></div>
+    <div class="bord2 d-none d-xl-block"></div>
+
+    <section id="home" class="d-none d-md-block">
+    <img src="img/home.png" style="width: 100vw;margin-top: -12vw;">
+    <img class=" home_shirt" src="img/website tshirt image 1.png">
+    <a class="btn btn2" href="register.php" target="_blank" role="button">BUY NOW</a>
+</section>
+
+<section id="home_mob" class="d-block d-md-none container text-center align-items-center">
+    <div class="merch text-center" style="margin-top: -8vw;opacity: 0.25;-webkit-text-stroke-width: 0.1vw;">MERCHANDISE</div>
+    <div class="merch text-center" style="margin-top: -10vw;opacity: 0.5;-webkit-text-stroke-width: 0.1vw;">MERCHANDISE</div>
+    <div class="merch text-center" style="margin-top: -10vw;color: #fff;">MERCHANDISE</div>
+    <img class=" home_mob_shirt" style="margin-top: -10vw;" src="img/website tshirt image 1.png">
+    <a class="btn btn2_mob text-center" href="register.php" target="_blank" role="button">BUY NOW</a>
+    <img style="margin-top: -10vw;margin-left: 20vw;" src="img/group.png" alt="">
+</section>
+
+<section class="container bg my-5 py-5 text-center align-items-center mx-auto" style="max-width: 85vw;">
+    <div class="row align-items-center mx-auto">
+        <div class="col-lg-6 col-12 my-4">
+            <img class="front px-xl-5" src="img/Tshirt_front.png" alt="">
+            <h4 class="text-center text-white sub my-4">FRONT</h4>
+        </div>
+        <div class="col-lg-6 col-12 my-4">
+            <img class="back px-xl-5" src="img/Tshirt-back complete(with name) 1.png" alt="">
+            <h4 class="text-center text-white sub my-4">BACK</h4>
+        </div>
+       
+        <div class="col-md-12 text-center my-2">
+            <a href="register.php" target="_blank"><button type="button" class="btn text-white btn3">BUY NOW</button></a>
+        </div>
+    </div>
+</section>
+
+<section class="container my-5 text-center align-items-center mx-auto">
+    <h4 class="heading d-block d-lg-none">OFFICIAL E-CELL MERCHANDISE</h4>
+    <div class="row align-items-center">
+        <div class="col-12 col-lg-6 my-4">
+            <img class="model" src="img/model.png" alt="">
+        </div>
+        <div class="col-12 col-lg-5 my-auto">
+            <h3 class="heading d-none d-lg-block my-2 text-left ml-xl-3">OFFICIAL E-CELL MERCHANDISE</h3>
+            <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 400;">Half Sleeve T-Shirt</h4>
+            <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 700;"><span class="mr-3"><i class="fas fa-rupee-sign"></i> 299 </span><span style="color: #919191;" class="strikethrough"><i class="fas fa-rupee-sign"></i> 499</span><span style="font-weight: 400;">(40% OFF)</span><span style="color: #919191;font-weight: 400;font-size: 1.3rem;"> Without Name</span></h4>
+            <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 700;"><span class="mr-3"><i class="fas fa-rupee-sign"></i> 349 </span><span style="color: #919191;" class="strikethrough"><i class="fas fa-rupee-sign"></i> 499</span><span style="font-weight: 400;">(30% OFF)</span><span style="color: #919191;font-weight: 400;font-size: 1.3rem;"> With Name</span></h4>
+            <h4 class="mt-5 mb-4 text-xl-left ml-xl-3" style="color: #919191;font-weight: 400;">Sizes Available</h4>
+            <div class="text-center d-xl-flex flex-row d-none sizebtn ml-xl-3" style="color: #919191;">
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 19px;">S</span>
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 17px;">M</span>
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 19px;">L</span>
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 15px;">XL</span>
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">2XL</span>
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">3XL</span>
+                <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">4XL</span>
+            </div>
+            <div class="row align-items-center d-flex justify-content-center d-lg-none" style="color: #919191;">
+                <div class="col-3">
+                    <span class="sizes" style="padding: 10px 19px;">S</span>
+                </div>
+                <div class="col-3">
+                    <span class="sizes" style="padding: 10px 19px;">M</span>
+                </div>
+                <div class="col-3">
+                    <span class="sizes " style="padding: 10px 19px;">L</span>
+                </div>
+                <div class="col-3">
+                    <span class="sizes" style="padding: 10px 13px;">XL</span>
+                </div>
+            </div>
+            <div class="row align-items-center d-flex justify-content-center d-lg-none" style="color: #919191;">
+                <div class="col-3  mt-4">
+                    <span class="sizes" style="padding: 10px 13px;">2XL</span>
+                </div>
+                <div class="col-3 mt-4">
+                    <span class="sizes" style="padding: 10px 13px;">3XL</span>
+                </div>
+                <div class="col-3 mt-4">
+                    <span class="sizes" style="padding: 10px 13px;">4XL</span>
+                </div>
+            </div>
+            <div class="col-md-12 text-xl-left my-5 ml-xl-3">
+                <a href="register.php" target="_blank"><button type="button" class="btn text-white btn3">BUY NOW</button></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container bg my-5 py-5 text-center align-items-center mx-auto">
+    <div class="row align-items-center mx-auto">
+        <div class="col-lg-6 col-12 my-4">
+            <img class="mask" src="img/mask.png" alt="">
+        </div>
+        <div class="col-lg-6 col-12 my-4">
+            <div class="group"></div>
+            <div class="text-xl-left free">FREE</div>
+            <div class="text-xl-left para">2 MASKS WITH EACH PURCHASE</div>
+        </div>
+    </div>
+</section>
+
+<section class="container bg my-5 py-5 text-center align-items-center mx-auto" style="max-width: 85vw;">
+    <h1 class="heading text-center">ALUMNI EDITION</h1>
+    <div class="row align-items-center mx-auto">
+        <div class="col-lg-6 col-12 my-4">
+            <img class="front px-xl-5 py-4" src="img/Tshirt-front complete(alumni) 1.png" alt="">
+            <h4 class="text-center text-white sub my-4">FRONT</h4>
+        </div>
+        <div class="col-lg-6 col-12 my-4">
+            <img class="back px-xl-5 py-4" src="img/Tshirt-back complete 1.png" alt="">
+            <h4 class="text-center text-white sub my-4">BACK</h4>
+        </div>
+       
+        <div class="col-md-12 text-center my-2">
+            <a href="register.php" target="_blank"><button type="button" class="btn text-white btn3">BUY NOW</button></a>
+        </div>
+    </div>
+</section>
+
+<section class="container align-items-center">
+    <h1 class="heading my-2 text-center">Size Chart</h1>
+    <img style="margin: 0 auto;" width="100%" src="img/chart.png" alt="">
+</section>
+</body>
+</html>
