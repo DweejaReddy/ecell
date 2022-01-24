a4ec6e5bffa283c16bd7cb0ba9d04729e7a287be
asachkahitari <patildeep2000@gmail.com>
2022-01-24 18:52:36 +0530

    Update store_details.php

--- merchandise/store_details.php
+++ merchandise/store_details.php
@@ -32,7 +32,7 @@ $_SESSION['email'] = $email;
         
     }
     else{
-        header('LOCATION:https://rzp.io/i/2QexfbTaVA');
+        header('LOCATION:https://rzp.io/l/B2YWU2P');
     }
     
   }
