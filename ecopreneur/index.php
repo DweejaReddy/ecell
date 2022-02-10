b655e59e4b0541d95ae41f2e87670dbfd2cc69ba
asachkahitari <patildeep2000@gmail.com>
2022-02-10 15:16:28 +0530

    Update index.php

--- ecopreneur/index.php
+++ ecopreneur/index.php
@@ -6,7 +6,7 @@
     <section id="home" class="d-none d-lg-block" style="margin-bottom: 13vw;">
         <div class="eco">ECOPRENEUR</div>
         <div class="subhead py-1">SUSTAIN THE FUTURE WITH INNOVATION</div>
-        <div class="para">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
+        <div class="para">It aims to incise the sharp minds of college students to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
         <div class="text-center">
         <a class="btn btn1" data-aos="fade-right"
   data-aos-duration="2000" href="img/Ecopreneur_brochure.pdf" target="_blank" role="button">Download Brochure</a>
@@ -25,7 +25,7 @@
             <div class="col-4"><img data-aos="fade-right"
   data-aos-duration="2000" style="background-color: #fff;border-radius: 20px;" src="img/logo.png" alt="" width="300px" ></div>
             <div class="col-6 text-left para1" data-aos="fade-left"
-  data-aos-duration="2000">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your NIT, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
+  data-aos-duration="2000">It aims to incise the sharp minds of college students to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your college, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
             <a class="btn btn3 py-2 px-3 mr-3" data-aos="fade-left"
   data-aos-duration="2000" href="img/Ecopreneur_brochure.pdf" target="_blank" role="button">Download Brochure</a>
             <a class="btn btn4 px-4 py-2 mx-2 " data-aos="fade-left"
@@ -49,7 +49,7 @@
   data-aos-duration="2000" style="height: auto;">
             <h4 class="text-left text-light font-weight-bold ml-3">PHASE 1: Ecowiz</h4>
             <br>
-                  <p class="text-left text-light ml-3" >A quiz centered around the topic of ecological sustainability will be conducted in all the participating NITS. Top 5 candidates from each college will be chosen to form a team and represent their respective NIT.</p>
+                  <p class="text-left text-light ml-3" >A quiz centered around the topic of ecological sustainability will be conducted in all the participating colleges. Top 5 candidates from each college will be chosen to form a team and represent their respective college.</p>
           </div>
         </div>
 
@@ -127,7 +127,7 @@
       <div class="text-center"><img src="img/eco.png" alt="" height="230vw"></div>
       <div class="text-center" style="font-size:18vw;font-family: 'Bebas Neue', cursive;color: #316835;margin-top: -4vw;">ECOPRENEUR</div>
       <div class="text-center px-2" style="margin: -4vw 18vw;color: #fff;background-color: #316835;font-size:3vw;">SUSTAIN THE FUTURE WITH INNOVATION</div>
-      <div class="text-center mt-4 mt-md-5" style="color: #316835;font-size: 3.4vw; margin: -4vw 18vw;">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
+      <div class="text-center mt-4 mt-md-5" style="color: #316835;font-size: 3.4vw; margin: -4vw 18vw;">It aims to incise the sharp minds of college students to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span></div>
       <div class="row align-items-center">
         <a class="btn btn1 py-2 mt-4 mt-md-5 col-5 " data-aos="fade-right"
 data-aos-duration="2000" href="img/Ecopreneur_brochure.pdf" target="_blank" role="button">Download Brochure</a>
@@ -144,7 +144,7 @@ data-aos-duration="2000" href="register.php" target="_blank" role="button">Regis
           <div class="row align-items-center">
             <div class="col-lg-4 col-12 text-center"><img style="background-color: #fff;border-radius: 20px;" src="img/logo.png" alt="" width="200px" ></div>
             <div class="col text-center para1" data-aos="fade-up"
-  data-aos-duration="2000" style="font-size: 3.7vw;margin-top: 5vw;">It aims to incise the sharp minds of NITIANs to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your NIT, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
+  data-aos-duration="2000" style="font-size: 3.7vw;margin-top: 5vw;">It aims to incise the sharp minds of college students to explore the world of <span style="font-weight: 900;">ENVIRONMENTAL ENTREPRENEURSHIP</span> by deducing a proper technical solution to an issue native to their institute's city and creating their own business model out of it. <br><br>Ecopreneur is chance to represent your college, it is an opportunity to solve a real world problem, a platform to represent a vision in front of notable people, a medium to win unimaginable prizes and recognition and a direction to maybe even get a fixed career path to follow out of the plan you create!</div>
             <div class="row text-center">
             <a class="btn btn3 py-2 px-5 my-4 mx-5 col" data-aos="fade-right"
   data-aos-duration="2000" href="img/Ecopreneur_brochure.pdf" target="_blank" role="button">Download Brochure</a>
@@ -167,7 +167,7 @@ data-aos-duration="2000" href="register.php" target="_blank" role="button">Regis
             <div class="col-7 event py-3" style="height: auto;" data-aos="fade-up"
   data-aos-duration="2000">
               <h4 class="text-left text-light font-weight-bold"style="font-size: 5vw;">PHASE 1: Ecowiz</h4>
-                  <p class="text-left text-light" style="font-size: 3.7vw;">A quiz centered around the topic of ecological sustainability will be conducted in all the participating NITS. Top 5 candidates from each college will be chosen to form a team and represent their respective NIT.</p>
+                  <p class="text-left text-light" style="font-size: 3.7vw;">A quiz centered around the topic of ecological sustainability will be conducted in all the participating colleges. Top 5 candidates from each college will be chosen to form a team and represent their respective college.</p>
             </div>
           </div>
 
