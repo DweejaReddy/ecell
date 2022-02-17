171ee5129150c24e6226f29871f873ddcfa45aae
asachkahitari <patildeep2000@gmail.com>
2022-02-17 10:44:29 +0530

    ceo added

---
+++ ceo/core.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('includes/header.php')?>
+<link rel="stylesheet" href="css/team.css">
+<title>Team | Consortium 22</title>
+</head>
+<body>
+  <section>
+    <div class="container heading mt-4 mb-1 py-5 text-center" style="max-width: 90%;">
+        <h1>OUR TEAM</h1>
+    </div>
+    
+    <div class="heading text-center my-5 container py-3 mx-auto" style="max-width: 90%;">
+      <div class="nav-bar row my-4">
+        <div class="nav-item col-4 "><a class="item" href="advisory.php">Advisory</a></div>
+        <div class="nav-item col-4"> <a class="item active" href="core.php">Core</a></div>
+        <div class="nav-item col-4"><a class="item" href="managers.php">Managers</a></div>
+      </div>
+
+        <!-- MEMBER 1 -->
+        <div class="row align-items-center">
+         <div class="col-12 col-md-6 col-lg-4 contactcard" style="height: 320px;">
+           <img src="img/team/bhatt-sir.jpeg" style="height: 220px;" alt="">
+           <h5 class="mt-3">DR. SS BHAT</h5>
+           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
+           <h6 class="my-2 desig">PROFESSOR INCHARGE</h6>
+         </div>
+         <!-- MEMBER 2 -->
+         <div class="col-12 col-md-6 col-lg-4 contactcard" style="height: 320px;">
+           <img src="img/team/taiwade-sir.jpeg" style="height: 220px;" alt="">
+           <h5 class="mt-3">DR. RV TAIWADE</h5>
+           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
+           <h6 class="my-2 desig">PROFESSOR INCHARGE</h6>
+         </div>
+          <!-- MEMBER 3 -->
+         <div class="col-12 col-md-6 col-lg-4 contactcard" style="height: 320px;">
+           <img src="img/team/Karthik_Balasundaram.webp" style="height: 220px;" alt="">
+           <h5 class="mt-3">Karthik Balasundaram</h5>
+           <hr style="width:35%;height:1.5px;color:red;opacity:1;margin:auto;">
+           <h6 class="my-2 desig">PROFESSOR INCHARGE</h6>
+         </div>
+        </div>
+        <div><img class="hexteam" src="img/hex.png" alt="" width="450px"></div>
+        <!-- MEMBER 4 -->
+        <div class="row align-items-center">
+         <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+           <div class="box">
+           <img src="img/team/PRESIDENT-team page.jpg" alt="">
+           <div class="info">
+             <div class="corename">Archis Bodkhe</div>
+             <div class="details">
+               <h6>President</h6><p>+91 97676 07235</p></div>
+             <div class="social-links">
+               <a href="https://www.linkedin.com/in/archis-bodkhe-655824160/"><i class="fa fa-linkedin"></i></a>
+               <a href="https://www.instagram.com/archisbodkhe/?hl=en"><i class="fa fa-instagram"></i></a>
+             </div>
+           </div>
+          </div>
+         </div>
+            
+         <!-- MEMBER 4 -->
+         <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+          <div class="box">
+            <img src="img/team/VP-team page.jpg" alt="">
+            <div class="info">
+              <div class="corename">Kanishka Meena</div>
+              <div class="details">
+                <h6>Vice President</h6><p>+91 93257 14868</p></div>
+              <div class="social-links">
+                <a href="https://www.linkedin.com/in/kanishka-meena-50a4a317a/"><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/_kanishka_0605/?hl=en"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+         </div>
+       </div>
+
+       
+
+       <div class="row align-items-center">
+        <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+          <div class="box">
+            <img src="img/team/bhargav-team page.jpg" alt="">
+            <div class="info pos1">
+              <div class="corename">Bhargav Choudhury</div>
+              <div class="details">
+                <h6>Treasurer</h6><p>+91 63761 28630<br>bhargavchoudhury071@gmail.com</p></div>
+              <div class="social-links">
+                <a href="https://www.linkedin.com/in/bhargav-choudhury-2371a7197/"><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/ft_bhrgv/"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+        </div>
+       </div>
+       <!-- MEMBER 6 -->
+       <div class="row align-items-center text-center mx-auto">
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/Rajat-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Rajat Patni</div>
+            <div class="details">
+              <h6>Head Of Corporate Relations</h6><p>+91 70280 81122<br>rajat.s.patni@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/rajat-patni-9508211a0/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/rajat_s_patni/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+       </div>
+       </div>
+       <!-- MEMBER 7 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/nikita-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Nikita Lohiya</div>
+            <div class="details">
+              <h6>Head of Corporate Relations</h6><p>+91 83908 63006<br>lohiyanikita01@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/nikita-lohiya-4b21161a1/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/_.nikkkkkkk_/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- MEMBER 8 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/Siddhesh-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Siddhesh Dumbre</div>
+            <div class="details">
+              <h6>Head Of Brand Development</h6><p>+91 70579 97990<br>sdumbre07@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/siddhesh-dumbre-b7aa76195/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/_siddd_07/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+
+       <!-- MEMBER 9 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/vanshika-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Vanshika Gupta</div>
+            <div class="details">
+              <h6>Head Of Brand Development</h6><p>+91 91668 45041<br>vg11072001@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/vanshikagupta-/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/vansi.ka_gupta/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+
+       <!-- MEMBER 10 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/bhavishya-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Bhavishya Jain </div>
+            <div class="details">
+              <h6>Head Of Events and Planning</h6><p>+91 91306 12265<br>bhavishyajn7@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/bhavishya-jain-212051196/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/bhavishya_jn7/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+
+       <!-- MEMBER 11 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/Tanvi-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Tanvi Khurana</div>
+            <div class="details">
+              <h6>Head Of Events and Planning</h6><p>+91 77578 20308<br>tksagi1212@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/tanvi-khurana-1149651b4/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/tanvi._khurana/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- MEMBER 12 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/jay-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Jay Chheda</div>
+            <div class="details">
+              <h6>Head Of Public Relations</h6><p>+91 98700 36145<br>jaychheda2410@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/jay-chheda-8739a0192/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/_jayyyyy.y/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- MEMBER 13 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/Snehal-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Snehal Patil</div>
+            <div class="details">
+              <h6>Head Of Public Relations</h6><p>+91 94231 32631<br>snehalpatil1801@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/snehal-patil-10b08a1a7/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/snehalll.19/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- MEMBER 14 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/prasen-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Prasen Kumar</div>
+            <div class="details">
+              <h6>Head Of Operations</h6><p>+91 62653 46554<br>prasenk05@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/prasen-kumar-b19b541b3/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/prasen_kumar/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- <span><img src="img/wireframe2.png" alt="" height="850px" width="400px" style="position: relative;bottom: -30px;margin-bottom: -800px;left: 70px;transform: rotate(100deg);opacity: 0.5;margin-top: -300px;"></span> -->
+       <!-- MEMBER 15 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/Mehul-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Mehul Madne</div>
+            <div class="details">
+              <h6>Head Of Operations</h6><p>+91 93076 27987<br>mmadne1999@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/mehul-madne-932446191/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/mehul_madne/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- MEMBER 16 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/Niharika-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Niharika Madke</div>
+            <div class="details">
+              <h6>Technical Head</h6><p>+91 97265 94357<br>niharikamadke@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/niharika-madke-5879721b4/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/_niharika1210/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <!-- MEMBER 17 -->
+       <div class="col-12 col-md-6 col-lg-4 my-4 corecard">
+        <div class="box">
+          <img src="img/team/deepak-team page.jpg" alt="">
+          <div class="info pos2">
+            <div class="corename">Deepak Patil</div>
+            <div class="details">
+              <h6>Technical Head</h6><p>+91 84850 95752<br>patildeep2000@gmail.com</p></div>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/deepak-patil-156390176/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/ultranoobpromaxx/"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+          </div>
+       </div>
+       <span><img src="img/wireframe2.png" alt="" height="850px" width="400px" style="position: relative;bottom: 290px;margin-bottom: -670px;left: -100px;transform: rotate(100deg);opacity: 0.4;"></span>
+
+      </div>
+</div>
+</section>
+<?php include('includes/footer.php')?>
+</body>
+</html>
