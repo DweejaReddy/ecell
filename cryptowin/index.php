f527bcaeec4e92d88f52b7501718acd907ccb396
asachkahitari <patildeep2000@gmail.com>
2022-02-22 20:21:22 +0530

    wallstreet and crypto template added

---
+++ cryptowin/index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CRYPTOWIN | Consortium '22, E-CELL, VNIT, 2022</title>
+</head>
+<?php include('includes/header.php')?>
+<link rel="stylesheet" href="css/eventStyle.css">
+<body>
+<hr style="border-top: 1px solid #fff;opacity: 0.8;margin-top: 3px;" class="mt-2">
+    <section class="d-none d-md-block my-5" id="home">
+        <div class="about row align-items-center py-4 mx-auto" style="max-width: 80%;">
+            
+    
+            <div class="col-12 col-md-5 text-white ml-4 pt-4">
+                <h1  style="font-weight: 900;text-shadow: 0px 1px, 1px 0px, 1px 1px;font-size: 4.5rem;" data-aos="fade-down" data-aos-duration="2000">CRYPTOWIN</h1>
+                <!-- <h5  style="font-weight: 500;text-shadow: 0px 1px, 1px 0px, 1px 1px;font-size: 1.3rem;" data-aos="fade-right" data-aos-duration="2000">UNCOVER THE FUTURE LEADERS</h5> -->
+                <p style="color: #D5242A;font-size: 1.4vw;">Win the challenge with the best Trades. <br>
+Implement your best strategies and win the best profits.
+
+
+                </p>
+                <!-- <p style="font-size: 1.2vw;" class="my-4">In this era of hyper-competition and non-stop disruption, the only way to stand out from the crowd is to stand for something unique. CEO is more than a competition; it’s a mission to decode future leaders; it’s a chance for students to show, test, and improve their leadership skills in a real-world setting. An event that provides a glimpse into the future, where excellent leadership is necessary for success, and where participants take risks and even thrive during the most challenging times. -->
+
+                  </p>
+                <a href="ceoReg.php"><button class="btn btn3 px-3" style="font-size: 1.4vw;" >Register Now for Free</button></a>
+                <a href="https://drive.google.com/file/d/16gdF-f04rbMDK5ogoHDiRiUc4BrlMtTS/view?usp=sharing" target="_blank"><button class="btn btn3 ml-2 my-2" style="font-size: 1.4vw;" >Brochure</button></a><br>
+                <p style="color: rgb(160, 160, 160);font-size: 1.3vw;" class="ml-5 mt-2">Last Date: 15th March</p></div>
+                  
+                <!-- <div class="col-12 col-md-5">
+                    <div class="owl-carousel owl-theme pt-5" id="one">
+                        <div class="item">
+                            <img src="img/ceo1.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo2.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo3.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo4.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo5.jpeg" alt="">
+                        </div>
+                    </div>
+                </div> -->
+        </div>
+    </section>
+
+    <section class="d-block d-md-none my-5" id="home_mob">
+        <div class="about row align-items-center py-4 mx-auto d-block d-lg-none">
+            <h1 class="text-center text-white mb-4"  style="font-weight: 900;font-size: 3.5rem;">CRYPTOWIN</h1>
+            <div class="col-12 col-md-5">
+                <div class="owl-carousel owl-theme" id="two">
+                <!-- <div class="item">
+                            <img src="img/ceo1.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo2.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo3.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo4.jpeg" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="img/ceo5.jpeg" alt="">
+                        </div>
+                </div> -->
+            </div>
+    
+            <div class="col-12 col-md-5 text-white text-center">
+                <!-- <h6  style="font-weight: 500;text-shadow: 0px 1px, 1px 0px, 1px 1px;font-size: 4.5vw;" class="mt-4">UNCOVER THE FUTURE LEADERS</h6> -->
+                <p style="color: #D5242A;font-size: 3.7vw;">Win the challenge with the best Trades. <br>
+Implement your best strategies and win the best profits.
+
+                </p>
+                <!-- <p style="color: rgb(160, 160, 160);font-size: 3.7vw;" class="text-center my-3" >In this era of hyper-competition and non-stop disruption, the only way to stand out from the crowd is to stand for something unique. CEO is more than a competition; it’s a mission to decode future leaders; it’s a chance for students to show, test, and improve their leadership skills in a real-world setting. An event that provides a glimpse into the future, where excellent leadership is necessary for success, and where participants take risks and even thrive during the most challenging times. -->
+
+                  </p>
+                <div class="text-center mt-3 px-5">
+                    <a href="ceoReg.php" target="_blank"><button type="button" class="btn btn3 my-2 px-3" style="font-size: 3.7vw;">Register Now for Free</button></a>
+                    <a href="https://drive.google.com/file/d/16gdF-f04rbMDK5ogoHDiRiUc4BrlMtTS/view?usp=sharing" target="_blank"><button type="button" class="btn btn3 my-2 px-5" style="font-size: 3.7vw;">Brochure</button></a>
+                    <p style="color: rgb(160, 160, 160);font-size: 4.2vw;" class=" mt-2">Last Date: 15th March</p></div>
+                </div></div>
+        </div>
+    </section>
+    <section class="my-5 text-center " id="struct">
+        <h1  style="font-weight: 900;font-size: 2.3rem;" class="text-white">EVENT STRUCTURE</h1>
+        <div class="row align-items-center">
+            <div class="col-1">
+                <div class="ceoE"><div class="ceoline1"></div><div class="ceoline2"></div></div>
+            </div>
+            <div class="col-7">
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">QUALIFYING ROUND </h4>
+                    <!-- <br> -->
+                    <p class="text-left ml-3">The qualifying round begins the moment you register, with the completion of a basic questionnaire covering corporate and financial topics. Keep in mind that you must submit your responses before we close registrations.
+Our experts will shortlist the contenders for the first round based on their critical thinking, decisiveness, business-related strategizing, and creative thinking.
+                    
+            </div>
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">ROUND 1: GROUP DISCUSSION</h4>
+                    <br>
+                  <p class="text-left ml-3">Industry experts will conduct a well-curated webinar for all participants, walking you through the best way to ace a group discussion. Then, in our group discussion round, we'll put your learnings and communication abilities to the test. The best performances in each group will progress to the next round.	                  
+            </div>
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">ROUND 2: MARKETISTAAN</h4>
+                    <br>
+                  <p class="text-left ml-3">Marketistaan is a market analysis case study round where participants qualifying the first round will then be divided into random teams to test the flexibility of the work environment. Different teams will compete against one another in providing the most practical corporate solution to the case study offered.
+                </p>
+                  
+            </div>
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">ROUND 3: THE BOARDROOM DILEMMA</h4>
+                    <br>
+                  <p class="text-left ml-3">Real problems require real answers, putting your analytical, problem-solving, and convincing abilities to the test. Participants in this round will showcase what it takes to be on the board of directors as well as to be the executive directors of a company that is facing a crucial decision. Make allies, understand your opponents, influence the neutral people, and present your point of view to the corporation's other stakeholders.
+                </p>
+                  
+            </div>
+        </div>
+        </div>
+        <span><img src="img/wireframe.png" alt="" width="1300px" style="position: relative;bottom: 1250px;z-index:-1;margin-bottom: -1280px;left: -10px;opacity: 0.7;"></span>
+    </section>
+    
+    <!-- <section>
+        <div class="text-center about mx-auto my-5 py-5" style="max-width: 80%;">
+            <h1  style="font-weight: 900;font-size: 2.3rem;color: #D5242A;">WHY PARTICIPATE?</h1>
+            <ul class="text-center reason mx-auto">
+                <li class="py-4 container">
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                </li>
+                <li class="py-2 container">
+                     Lorem Ipsum has been the industry's standard dummy text ever since the
+                </li>
+                <li class="py-4 container">
+                     1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
+                </li>
+            </ul>
+        </div>
+    </section> -->
+    <section class="d-none d-lg-block mx-auto text-center" style="max-width: 90%;" id="timeline">
+        <h1  style="font-weight: 900;font-size: 2.3rem;" class="text-white my-4">TIMELINE</h1>
+        <div id="horizontal-timeline" class="timeline horizontal reverse">
+            <div class="block">
+              <div class="square up" data-aos="fade-down" data-aos-duration="2000">
+                <h3>REGISTRATION CUM QUALIFYING ROUND</h3>
+                <h4>17 Feb-15 March</h4>
+              </div>
+              <div class="circle">
+                <p>1</p>
+              </div>
+              <div class="link"></div>
+              <div class="square down blank"></div>
+            </div>
+            <div class="block">
+              <div class="square up blank"></div>
+              <div class="circle activated">
+                <p>2</p>
+              </div>
+              <div class="link"></div>
+              <div class="square down" data-aos="fade-up" data-aos-duration="2000">
+                <h3>WEBINAR FOR ROUND 1</h3>
+                <h4>19th March</h4>
+              </div>
+            </div>
+            <div class="block">
+              <div class="square up" data-aos="fade-down" data-aos-duration="2000">
+                <h3>ROUND 1: GROUP DISCUSSION</h3>
+                <h4>20th March</h4>
+              </div>
+              <div class="circle">
+                <p>3</p>
+              </div>
+              <div class="link"></div>
+              <div class="square down blank"></div>
+            </div>
+            <div class="block">
+              <div class="square up blank"></div>
+              <div class="circle">
+                <p>4</p>
+              </div>
+              <div class="link"></div>
+              <div class="square down" data-aos="fade-up" data-aos-duration="2000">
+                <h3>WORKSHOP</h3>
+                <h4>2nd April</h4>
+              </div>
+            </div>
+          
+            <div class="block">
+              <div class="square up" data-aos="fade-down" data-aos-duration="2000">
+                <h3>ROUND 2: MARKETISTAAN</h3>
+                <h4>3rd April</h4>
+              </div>
+              <div class="circle">
+                <p>5</p>
+              </div>
+              <div class="link"></div>
+              <div class="square down blank"></div>
+            </div>
+
+            <div class="block">
+                <div class="square up blank"></div>
+                <div class="circle">
+                  <p>6</p>
+                </div>
+                <div class="link"></div>
+                <div class="square down" data-aos="fade-up" data-aos-duration="2000">
+                  <h3>ROUND 3: THE BOARDROOM DILEMMA</h3>
+                  <h4>9th April</h4>
+                </div>
+              </div>
+            
+            <div id="end"></div>
+          </div>
+    </section>
+
+    <section class="d-block d-lg-none mx-auto text-center" style="max-width: 90%;" id="timeline_mob">
+        <h1  style="font-weight: 900;font-size: 2.3rem;" class="text-white my-5">TIMELINE</h1>
+        <div id="vertical-timeline" class="timeline vertical">
+            <div class="block">
+              <div class="square right vert"data-aos="fade-right" data-aos-duration="2000" >
+                <h3>REGISTRATION CUM QUALIFYING ROUND</h3>
+                <h4>17 Feb-15 March</h4>
+              </div>
+              <div class="circle" style="left: 27px;margin-left: -18px;">
+                <p>1</p>
+              </div>
+              <div class="link"></div>
+              <div class="square left blank"></div>
+            </div>
+          
+            <div class="block">
+              <div class="square right blank"></div>
+              <div class="circle" style="left: 27px;margin-left: -18px;">
+                <p>2</p>
+              </div>
+              <div class="link"></div>
+              <div class="square left vert" data-aos="fade-left" data-aos-duration="2000">
+                <h3>WEBINAR FOR ROUND 1</h3>
+                <h4>19th March</h4>
+              </div>
+            </div>
+          
+            <div class="block">
+              <div class="square right vert" data-aos="fade-right" data-aos-duration="2000">
+                <h3>ROUND 1: GROUP DISCUSSION</h3>
+                <h4>20th March</h4>
+              </div>
+              <div class="circle activated" style="left: 27px;margin-left: -18px;">
+                <p>3</p>
+              </div>
+              <div class="link"></div>
+              <div class="square left blank"></div>
+            </div>
+          
+            <div class="block">
+              <div class="square right blank"></div>
+              <div class="circle" style="left: 27px;margin-left: -18px;">
+                <p>4</p>
+              </div>
+              <div class="link"></div>
+              <div class="square left vert" data-aos="fade-left" data-aos-duration="2000">
+                <h3>WORKSHOP</h3>
+                <h4>2nd April</h4>
+              </div>
+            </div>
+          
+            <div class="block">
+              <div class="square right vert" data-aos="fade-right" data-aos-duration="2000">
+                <h3>ROUND 2: MARKETISTAAN</h3>
+                <h4>3rd April</h4>
+              </div>
+              <div class="circle" style="left: 27px;margin-left: -18px;">
+                <p>5</p>
+              </div>
+              <div class="link"></div>
+              <div class="square left blank"></div>
+            </div>
+
+            <div class="block">
+                <div class="square right blank"></div>
+                <div class="circle" style="left: 27px;margin-left: -18px;">
+                  <p>6</p>
+                </div>
+                <div class="link"></div>
+                <div class="square left vert" data-aos="fade-left" data-aos-duration="2000">
+                  <h3>ROUND 3: THE BOARDROOM DILEMMA</h3>
+                  <h4>9th April</h4>
+                </div>
+              </div>
+            <div id="end"></div>
+          </div>
+    </section>
+
+    <section>
+        <div class="text-center about cont text-white mx-auto my-5 py-5" style="max-width: 80%;" id="managers">
+            <h1  style="font-weight: 900;font-size: 2.3rem;color: #D5242A;">Contact Us</h1>
+            <p class="px-3 my-4">For any event related queries, do reach out to us</p>
+            <div class="row align-items-center">
+                <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+                    <h5 class="mt-2 pt-3">Aryan Mahure</h5>
+                    <a class="ml-4" href="https://www.linkedin.com/in/aryan-mahure-5ba135112" target="_blank" >
+                        <img id="linkedin" src="img/linkedin_black.png" alt="">
+                    </a>
+                    <!-- <div class="name">bhargav@gmail.com</div> -->
+                    <div class="phone pb-4" style="margin-top: -30px;">+91 93565 14618</div>
+                </div>
+                <!-- <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+                    <h5 class="mt-2 pt-3">Sourabh Bedkute</h5>
+                        <img id="linkedin" src="img/linkedin_black.png" alt="">
+                    <div class="phone pb-4" style="margin-top: -30px;">+91 91563 77253</div>
+                </div> -->
+                <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+                    <h5 class="mt-2 pt-3">Tarun Saraf</h5>
+                    <a class="ml-4" href="https://www.linkedin.com/in/tarun-saraf-2107" target="_blank" >
+                        <img id="linkedin" src="img/linkedin_black.png" alt="">
+                    </a>
+                    <div class="phone pb-4" style="margin-top: -30px;">+91 96913 61448</div>
+                </div>
+                <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+                    <h5 class="mt-2 pt-3">Samruddhi Wagh</h5>
+                    <a class="ml-4" href="https://www.linkedin.com/in/samruddhi-wagh-96096b218" target="_blank" >
+                        <img id="linkedin" src="img/linkedin_black.png" alt="">
+                    </a>
+                    <div class="phone pb-4" style="margin-top: -30px;">+91 78418 60535</div>
+                </div>
+                <!-- <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+                    <h5 class="mt-2 pt-3">Tanishqa Mishra</h5>
+                    <a class="ml-4" href="https://www.linkedin.com/in/tanishqa-mishra-406ab5209/" target="_blank" >
+                        <img id="linkedin" src="img/linkedin_black.png" alt="">
+                    </a>
+                    <div class="phone pb-4" style="margin-top: -30px;">+968 99120105</div>
+                </div> -->
+            </div>
+        </div>
+    </section>
+    <?php include('includes/footer.php')?>
+</body>
+</html>
