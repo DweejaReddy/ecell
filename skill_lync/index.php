3ad84a4d2018a317d4855d63c93710be75afd3c2
Dweeja Reddy <dweeja54@gmail.com>
2022-07-03 03:29:05 +0530

    skill-lync added

---
+++ skill_lync/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <title>Download Skill-Lync Workshop certificate</title>
+    <link rel="stylesheet" href="certstyle.css">
+    <link rel="favicon" href="favicon.ico">
+  </head>
+
+  <body style="vertical-align: middle;">
+    <header>
+        <img src="VectorU.png" width="100%" alt="uppercurve">
+    </header>
+    <div class="group text-center text-white">
+      <img src="ecell.png" width="20%" class="ecell" alt="logo">
+        <h3 class="mt-3" style="color: #0083FF;font-weight: 700;font-size: 1.5rem;">Download your participation certificate</h3>
+    <main class="text-center my-3 text-white">
+      <form method="post" action="filter.php">
+        <label for="name">Enter Your Name</label><br>
+        <input class="my-3 text-center" required type="text" name="name" autocomplete="name" placeholder="Name"
+          id="name" minlength="3"><br>
+        <label for="name">Enter Your Registered Phone number</label><br>
+        <input class="my-3 text-center" required type="text" name="contact" placeholder="Phone number" required><br>
+        <Button id="submitBtn" type="submit" class="btn btnth mt-3 mb-5" name="certificate_form">Download Certificate</Button>
+        <form>
+    </main>
+    <div class="text-center text-white D">
+      <img src="VectorD2.png" class="D2" width="100%" alt="lowergraycurve">
+      <img src="VectorD1.png" class="D1" width="100%" alt="lowerbluecurve">
+    </div>
+  </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+      integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+      crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+      integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+      crossorigin="anonymous"></script>
+  </body>
+
+</html>
---
+++ skill_lync/nouser.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Thank You</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- external css  -->
+    <link rel="stylesheet" href="certstyle.css">
+    <!-- Web Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!-- favicon -->
+    <!-- <link rel = "icon" href = "images/icon.png" type = "image/x-icon"> -->
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon_io/site.webmanifest">
+    </head>
+
+    <style>
+      .text{
+        margin-top:-200px;
+      }
+      @media screen and (max-width:767px){
+        .text{
+          margin-top: -60px;
+        }
+      }
+    </style>
+  <body>
+  <header>
+        <img src="VectorU.png" width="100%" alt="uppercurve">
+    </header>
+        <div class="" >
+        <div class="container d-flex align-items-center justify-content-center min-vh-100">
+            <div class="row text-center text">
+                <div class="text my-4" style="color:white;">
+                  <?php echo "<h4>Dear Student,<br>Please<span style=color:#0083FF> verify your registered mobile number</span> you have entered.<br>If you have <span style=color:#0083FF>successfully</span> attended the Skill-Lync Full Stack Web Development workshop of E-CELL, VNIT and still not able to get your certificate, mail your query at <a style=color:#0083FF;text-decoration:none href=mailto:neo.pr.ecellvnit@gmail.com> contact@ecellvnit.org</a><br><br>Do visit E-CELL, VNIT website for regular updates";?><br><br>
+                  <a href="https://www.ecellvnit.org/" target="_blank"> 
+              <button class="btn btnth">Visit E-CELL Website</button></a><br><br>
+              
+                </div>
+            </div>
+            <!-- <br> -->
+        </div>
+        </div>
+
+    <div class="text-center text-white D">
+      <img src="VectorD2.png" class="D2" width="100%" alt="lowergraycurve">
+      <img src="VectorD1.png" class="D1" width="100%" alt="lowerbluecurve">
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+<!-- <h1>Your have already registered,<b style=color:#ff8606> $name!</b><br> </h1><h4>You will receive the study material and examination admit card soon through <span style=color:#ff8606>Email</span> and <span style=color:#ff8606>WhatsApp medium<br></h4><h4>If you still have not joined the community, JOIN NOW to know different facts of Entrepreneurship.<br> <span style=color:#ff8606>Link is also on the home page </span></h4> -->
