0a80a4dea922a3859d8837805ed03e8106e9823f
Dweeja Reddy <dweeja54@gmail.com>
2022-07-22 20:28:46 +0530

    Update index.php

--- flagshipfinal/index.php
+++ flagshipfinal/index.php
@@ -119,7 +119,14 @@
   <section id="speakers">
     <div class="container-fluid text-center mt-5">
       <div class="heading mt-3 mb-5">Speakers <span style="color: #FF4200;">Flagship ❛22</span></div>
-      <div class="coming-soon my-5">COMING SOON</div>
+      <div class="row">
+      <div class="col-12 mb-5">
+        <img src="img/Aman sir.jpg" style="border: 7px solid #FF4200;border-radius: 50%;" height="250px" width="250px" alt="">
+        <p class="mt-3" style="color: #FF4200;font-size: 1.3rem;font-weight: 500;">Aman Dhattarwal</p>
+        <p class="mb-5" style="font-size: 1rem;font-weight: 500;">Founder, Apni Kaksha & Apna College</p>
+      </div>
+      </div>
+    </div>
     </div>
   </section>
 
